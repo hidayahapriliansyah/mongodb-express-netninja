51e6a457426f8d9e38560f0f721aaa2387ef8f8e
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-30 10:11:04 +0700

    cursor and fetching data

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const { connectToDb, getDb } = require('./db');
 
 // init and middleware
 const app = express();
 
-app.listen(3000, () => {
-  console.log('app listening on port 3000');
+let db; 
+
+// dbconnection 
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log('app listening on http://localhost:3000');
+    });
+    db = getDb();
+  }
 });
 
 // routes
 app.get('/books', (req, res) => {
-  res.json({ message: 'Welcome to the api' });
+  let books = [];
+
+  db.collection('books')
+    .find() // find itu mereturn cursor, 
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
