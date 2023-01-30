e39c8259efcdc0c22f26eb06f809b30ed14b9180
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-30 12:45:23 +0700

    handling delete request

--- app.js
+++ app.js
@@ -70,3 +70,18 @@ app.post('/books', (req, res) => {
       res.status(500).json({ error: 'Could not insert book'});
     });
 });
+
+app.delete('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: 'Could not delete the document' });
+      })
+  } else {
+    res.status(500).json({ error: 'Not valid id' });
+  }
+});
